aab443d17f38e19f300fa8c1380fd1632678f984
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-30 14:48:04 +0700

    Optimizes scripts' runner

--- datanode/run.sh
+++ datanode/run.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
-if [ ! -d $datadir ]; then
-  echo "Datanode data directory not found: $datadir"
-  exit 2
-fi
 
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
+function ping_service {
+    HOST=$1
+    PORT=$2
+    TIMEOUT=$3
+
+    until nc -w $TIMEOUT -z $HOST $PORT; do
+        echo "Connection to ${HOST}:${PORT} was failed"
+        sleep 1
+    done
+
+    echo "${HOST} is ready to be used!"
+}
+
+
+function run_datanode {
+    echo "Waiting for 'namenode' to be ready..."
+    ping_service "namenode" "9870" "60"
+
+    datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+    if [ ! -d $datadir ]; then
+        echo "Datanode data directory not found: $datadir"
+        exit 2
+    fi
+
+    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
+}
+
+
+# Main
+run_datanode
--- resourcemanager/run.sh
+++ resourcemanager/run.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
+
+function ping_service {
+    HOST=$1
+    PORT=$2
+    TIMEOUT=$3
+
+    until nc -w $TIMEOUT -z $HOST $PORT; do
+        echo "Connection to ${HOST}:${PORT} was failed"
+        sleep 1
+    done
+
+    echo "${HOST} is ready to be used!"
+}
+
+
+function run_resourcemanager {
+    echo "Waiting for 'namenode' to be ready..."
+    ping_service "namenode" "9870" "60"
+
+    echo "Waiting for 'datanode1' to be ready..."
+    ping_service "datanode1" "9864" "60"
+
+    echo "Starting to run 'resourcemanager' service..."
+    $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
+}
+
+
+# Main
+run_resourcemanager
