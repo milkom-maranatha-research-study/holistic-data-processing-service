e09afda240c1cd7e2fed4b909b2ff5c06764856f
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-06 21:18:11 +0700

    Simplifies `TherapistInteractionBackendOperation`

--- data-processing-service/data_processor/sources/operations.py
+++ data-processing-service/data_processor/sources/operations.py
@@ -15,6 +15,19 @@ class TherapistInteractionBackendOperation:
 
     def __init__(self) -> None:
         self.api = TherapistInteractionAPI()
+    
+    @property
+    def data(self) -> dask_dataframe:
+        """
+        Returns the Therapist Interaction dask's dataframe.
+        """
+
+        assert hasattr(self, '_ddf'), (
+            'Dask dataframe is not available!\n'
+            'You must call `.collect_data()` first.'
+        )
+
+        return self._ddf
 
     def collect_data(self) -> None:
         """
@@ -28,4 +41,16 @@ class TherapistInteractionBackendOperation:
         if not settings.DEV_MODE:
             self.api.download_data(format='csv')
 
-        self.ddf = dask_dataframe.read_csv(self.api._BE_THER_INTERACTIONS_FILE, parse_dates=['interaction_date', 'organization_date_joined'])
+        self._ddf = dask_dataframe.read_csv(
+            self.api._BE_THER_INTERACTIONS_FILE,
+            dtype={
+                'interaction_id': 'Int64',
+                'therapist_id': str,
+                'chat_count': 'Int64',
+                'call_count': 'Int64',
+                'interaction_date': str,
+                'organization_id': 'Int64',
+                'organization_date_joined': str
+            },
+            parse_dates=['interaction_date', 'organization_date_joined']
+        )
