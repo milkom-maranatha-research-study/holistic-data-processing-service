100ca5051354968e274bf67bade9fc74c4844ba1
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-07 08:55:55 +0700

    Fixed doc-strings

--- data-processing-service/data_processor/main.py
+++ data-processing-service/data_processor/main.py
@@ -21,31 +21,34 @@ class TherapistInteractionProcessor:
         Process therapists's interaction data from Backend and export it into multiple CSV files.
         """
 
-        # Stage 1 - Collect therapists' interaction data from Backend.
+        # Step 1 - Collect Data
+        # * Get therapists' interaction data from the Backend.
         self.backend_therapist_interaction.collect_data()
         dataframe = self.backend_therapist_interaction.data
 
-        # Stage 2 - Excludes therapists' interaction rows that doesn't have the Organization ID
-        # * We consider those rows as dirty data, because every therapist-user must be a member of an Organization.
+        # Step 2 - Clean Data
+        # * 2.1 Delete rows that doesn't have the Organization ID
+        # *     We consider those rows as dirty data, because every therapist-user must be a member of an Organization.
         dataframe = dataframe.dropna(subset=['organization_id'])
 
-        # Stage 3 - Excludes therapists' interaction rows that has `chat_count` <= 1 and `call_count` < 1.
-        # * We assumes that:
-        # * a. Therapists' interaction is valid when they send a chat to their clients more than once.
-        # *    It means they replied to the client's chat message. OR
-        # * b. Therapists' interaction is valid when they have a call with the client at least once.
-        # *    It means the therapist and the client talked to each other.
+        # * 2.2 Delete rows that has `chat_count` <= 1 and `call_count` < 1.
+        # *     We assume those rows are invalid, consider that:
+        # *     a. Therapist's interaction is valid when they send a chat to their clients more than once.
+        # *        It means they replied to the client's chat message. OR
+        # *     b. Therapist's interaction is valid when they have a call with the client at least once.
+        # *        It means the therapist and the client talked to each other.
         dataframe = dataframe[(dataframe['chat_count'] > 1) | (dataframe['call_count'] >= 1)]
 
-        # Stage 4 - Distinct rows based on the therapist ID and interaction date.
-        # * We assumes that if the therapist interacts with multiple clients in the same day, we only need to pick one.
+        # Step 3 - Data Distinction
+        # * We need to distinct the data rows based on the therapist ID and interaction date.
+        # * We assume that if the therapist interacts with multiple clients in the same day, we only need to pick one.
         # * It's sufficient (for now) to tells that therapist is active on that day.
         dataframe = dataframe.drop_duplicates(subset=['therapist_id', 'interaction_date'], keep='last')
 
-        # Stage 5 - Executes every task from the previous stages.
+        # Step 4 - Executes every task from the previous steps.
         dataframe = dataframe.compute()
 
-        # Final Stage - Export results into multiple CSV file!
+        # Step 5 - Export results into multiple CSV file!
         self._create_input_dir()
         dataframe.to_csv(f'{self.INPUT_PATH}/thers-interactions.csv', header=False, index=False)
 
