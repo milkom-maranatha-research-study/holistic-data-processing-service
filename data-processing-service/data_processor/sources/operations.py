2e3a5617b10f4f0c0330ea1bb548816bd3233b2c
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-23 15:23:08 +0700

    Modifies `data_processor` operation classes - Introduces `TherapistBackendOperation` class - Modifies BE endpoint on the `InteractionBackendOperation` class

--- data-processing-service/data_processor/sources/operations.py
+++ data-processing-service/data_processor/sources/operations.py
@@ -3,18 +3,57 @@ import settings
 
 from dask import dataframe as dask_dataframe
 
-from sources.clients import TherapistInteractionAPI
-from sources.dateutils import DateUtil
+from sources.clients import InteractionAPI, TherapistAPI
 
 
 logger = logging.getLogger(__name__)
 
 
-class TherapistInteractionBackendOperation:
-    dateutil = DateUtil()
+class TherapistBackendOperation:
 
     def __init__(self) -> None:
-        self.api = TherapistInteractionAPI()
+        self.api = TherapistAPI()
+    
+    @property
+    def data(self) -> dask_dataframe:
+        """
+        Returns the Therapist Interaction dask's dataframe.
+        """
+
+        assert hasattr(self, '_ddf'), (
+            'Dask dataframe is not available!\n'
+            'You must call `.collect_data()` first.'
+        )
+
+        return self._ddf
+
+    def collect_data(self) -> None:
+        """
+        Download and validates data from Backend if the Developer Mode is off.
+
+        Otherwise, we load the temporary CSV file from disk.
+        """
+
+        logger.info("Collecting data from Backend or importing from disk...")
+
+        if not settings.DEV_MODE:
+            self.api.download_data(format='csv')
+
+        self._ddf = dask_dataframe.read_csv(
+            self.api._BE_THERAPISTS_FILE,
+            dtype={
+                'id': str,
+                'organization_id': 'Int64',
+                'date_joined': str
+            },
+            parse_dates=['date_joined']
+        )
+
+
+class InteractionBackendOperation:
+
+    def __init__(self) -> None:
+        self.api = InteractionAPI()
     
     @property
     def data(self) -> dask_dataframe:
@@ -44,11 +83,11 @@ class TherapistInteractionBackendOperation:
         self._ddf = dask_dataframe.read_csv(
             self.api._BE_THER_INTERACTIONS_FILE,
             dtype={
-                'interaction_id': 'Int64',
                 'therapist_id': str,
+                'interaction_date': str,
+                'counter': 'Int64',
                 'chat_count': 'Int64',
                 'call_count': 'Int64',
-                'interaction_date': str,
                 'organization_id': 'Int64',
                 'organization_date_joined': str
             },
