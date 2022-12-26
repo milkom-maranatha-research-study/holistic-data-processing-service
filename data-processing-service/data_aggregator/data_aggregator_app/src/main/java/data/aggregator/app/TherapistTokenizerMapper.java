38053bf917b7b34ab9d78b747aa6f2c112e54531
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-26 15:55:24 +0700

    Create mapper class to map active/inactive therapists

---
+++ data-processing-service/data_aggregator/data_aggregator_app/src/main/java/data/aggregator/app/TherapistTokenizerMapper.java
@@ -0,0 +1,74 @@
+package data.aggregator.app;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+/**
+ * A mapper class that implements in-mapper combiner algorithm.
+ * 
+ * All incoming `text` will be calculated before we sent it to the Reducer class.
+ */
+public class TherapistTokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+	private static Map<String, Integer> map = new HashMap<String, Integer>();
+
+	/**
+	 * Calculate every unique token and put it on the class's map object.
+	 * If the token doesn't exists, initialize it with 1. Otherwise, sum it with 1.
+	 */
+	@Override
+	protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		StringTokenizer itr = new StringTokenizer(value.toString());
+ 
+		while (itr.hasMoreTokens()) {
+			String token = itr.nextToken();
+			
+			// Extracts tokens by removing comma delimiter
+			// Input is in this format "{period},{orgId},{totalThersInOrg},{therId}"
+			String[] tokens = token.split(",");
+
+			// Construct a new key
+			String keyValue = String.format("%s,%s,%s", tokens[0], tokens[1], tokens[2]); 
+
+			if(map.containsKey(keyValue)) {
+				int totalActiveThers = map.get(keyValue).intValue() + 1;
+
+				map.put(token, totalActiveThers);
+
+				continue;
+			}
+
+			map.put(keyValue, 1);
+		}
+	}
+
+	/**
+	 * We override the `cleanup` method to write every map object into HDFS
+	 * through the mapper's context.
+	 */
+	@Override
+	protected void cleanup(Context context) throws IOException, InterruptedException {
+
+		Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+
+		while(iterator.hasNext()) {
+			Map.Entry<String, Integer> entry = iterator.next();
+
+			String keyValue = entry.getKey();
+
+			int totalActiveThers = entry.getValue().intValue();
+
+			context.write(new Text(keyValue), new IntWritable(totalActiveThers));
+		}
+
+		map.clear();
+	}
+}
