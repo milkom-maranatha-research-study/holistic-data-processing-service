9bcf5c0945a5a47ead44e6ce555e1bfb2ec97036
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-26 14:48:43 +0700

    Rework main data processor

--- data-processing-service/data_processor/main.py
+++ data-processing-service/data_processor/main.py
@@ -16,8 +16,15 @@ from sources.operations import (
 logger = logging.getLogger(__name__)
 
 
+NUM_OF_THERS_INPUT_PATH = 'input/num_of_ther'
+NUM_OF_THERS_FILENAME = 'number-of-therapist'
+NUM_OF_THERS_PER_ORG_FILENAME = 'number-of-therapist-per-org'
+
+INTERACTION_INPUT_PATH = 'input/interaction'
+INTERACTION_FILENAME = 'interaction'
+
+
 class TherapistDataProcessor:
-    INPUT_PATH = 'input/num_of_thers'
 
     def __init__(self) -> None:
         self.therapist_operation = TherapistBackendOperation()
@@ -56,7 +63,7 @@ class TherapistDataProcessor:
         logger.info("Count total therapists per Organization...")
         # * 3.2 Count the number of therapist per Organization.
         grouped_dataframe = cleaned_dataframe.groupby(['organization_id'])['id'].count()\
-            .compute().reset_index(name='total_thers')
+            .compute().reset_index(name='total_thers_in_org')
 
         # Step 4 - Save results into CSV files
         self._to_csv(num_all_therapist, grouped_dataframe)
@@ -69,10 +76,10 @@ class TherapistDataProcessor:
         """
         Saves that `num_all_therapist` and `grouped_dataframe` into CSV files.
         """
-        is_exists = os.path.exists(self.INPUT_PATH)
+        is_exists = os.path.exists(NUM_OF_THERS_INPUT_PATH)
 
         if not is_exists:
-            os.makedirs(self.INPUT_PATH)
+            os.makedirs(NUM_OF_THERS_INPUT_PATH)
 
         # Write total therapists in NiceDay
         logger.info("Save total therapists in NiceDay into CSV file...")
@@ -80,7 +87,7 @@ class TherapistDataProcessor:
         header = ['total_thers']
         data = [num_all_therapist]
 
-        with open(f'{self.INPUT_PATH}/number-of-therapist.csv', 'w') as file:
+        with open(f'{NUM_OF_THERS_INPUT_PATH}/{NUM_OF_THERS_FILENAME}.csv', 'w') as file:
             writer = csv.writer(file)
             writer.writerow(header)
             writer.writerow(data)
@@ -89,15 +96,15 @@ class TherapistDataProcessor:
         logger.info("Save total therapists per Organization into CSV files...")
 
         grouped_dataframe.to_csv(
-            f'{self.INPUT_PATH}/number-of-therapist-per-org.csv',
+            f'{NUM_OF_THERS_INPUT_PATH}/{NUM_OF_THERS_PER_ORG_FILENAME}.csv',
             index=False
         )
 
 
 class InteractionDataProcessor:
-    INPUT_PATH = 'input/interaction'
 
     def __init__(self) -> None:
+        self.therapist_operation = TherapistBackendOperation()
         self.ther_interaction_operation = InteractionBackendOperation()
 
         # Runs data processor
@@ -114,31 +121,31 @@ class InteractionDataProcessor:
         and writes it into multiple CSV files.
         """
 
-        # Step 1 - Collect Data
+        # Step 1 - Data Collection
         # * Get therapists' interactions from the Backend.
         self.ther_interaction_operation.collect_data()
         dataframe = self.ther_interaction_operation.data
 
-        logger.info("Cleaning data...")
-        # Step 2 - Clean Data
+        # Step 2 - Data Cleaning
         # * 2.1 Delete rows that doesn't have the Organization ID
         # *     For now, we consider those rows as dirty data,
         # *     We assume every therapist must be a member of the Organization.
+        logger.info("Removing rows with no Organization ID...")
         dataframe = dataframe.dropna(subset=['organization_id'])
 
         # * 2.2 Delete rows that has `chat_count` <= 1 and `call_count` < 1.
         # *     We assume those rows are invalid, consider that:
-        # *     a. Therapist's interaction is valid when they send a chat
+        # *     a. Interaction is valid when the therapist send a chat
         # *        to their clients more than once.
         # *        It means they replied to the client's chat message. OR
-        # *     b. Therapist's interaction is valid when they have a call
+        # *     b. Interaction is valid when the therapist have a call
         # *        with the client at least once.
         # *        It means the therapist and the client talked to each other.
+        logger.info("Removing invalid interactions...")
         dataframe = dataframe[
             (dataframe['chat_count'] > 1) | (dataframe['call_count'] >= 1)
         ]
 
-        logger.info("Distinct data by interaction date...")
         # Step 3 - Data Distinction
         # * We need to distinct the data rows based on the therapist ID
         # * and interaction date.
@@ -147,68 +154,91 @@ class InteractionDataProcessor:
         # * in the same day, we only need to pick one.
         # 
         # * It's sufficient (for now) to tells that therapist is active on that day.
-        cleaned_dataframe = dataframe.drop_duplicates(
+        logger.info("Distinct data by interaction date...")
+        dataframe = dataframe.drop_duplicates(
             subset=['therapist_id', 'interaction_date'],
             keep='last'
         )
 
-        # Step 4 - Save results into CSV files
-        is_exists = os.path.exists(self.INPUT_PATH)
+        # Step 4 - Merge interaction data with the therapist data
+        # * We need to merge interaction dataframe with the therapist dataframe
+        # * to generate a new column called `total_therapists_in_org`.
+        # *
+        # * This is required by the aggregator service, so it can calculate the number
+        # * of active/inactive therapists per org in one go.
+        logger.info("Merge interaction dataframe with the therapist dataframe...")
+        total_therapists_dataframe = dask_dataframe.read_csv(
+            f'{NUM_OF_THERS_INPUT_PATH}/{NUM_OF_THERS_PER_ORG_FILENAME}.csv',
+            dtype={
+                'organization_id': 'Int64',
+                'total_thers_in_org': 'Int64'
+            }
+        )
 
-        if not is_exists:
-            os.makedirs(self.INPUT_PATH)
+        dataframe = dataframe.merge(
+            total_therapists_dataframe,
+            how='left',
+            on='organization_id'
+        )
+
+        # Step 5 - Save the final data into CSV files
+        # Weekly period
+        # * Generate a weekly `period` column based on the `interaction_date`.
+        weekly_dataframe = dataframe.assign(
+            period=lambda x: x.interaction_date.dt.to_period('W')
+        )
 
         logger.info("Save Interaction data in weekly period into CSV files...")
-        self._to_csv(cleaned_dataframe, 'W', 'weekly')
+        self._to_csv(weekly_dataframe, 'weekly')
+
+        # Monthly period
+        # * Generate a monthly `period` column based on the `interaction_date`.
+        monthly_dataframe = dataframe.assign(
+            period=lambda x: x.interaction_date.dt.to_period('M')
+        )
 
         logger.info("Save Interaction data in monthly period into CSV files...")
-        self._to_csv(cleaned_dataframe, 'M', 'monthly')
+        self._to_csv(monthly_dataframe, 'monthly')
+
+        # Yearly period
+        # * Generate a yearly `period` column based on the `interaction_date`.
+        yearly_dataframe = dataframe.assign(
+            period=lambda x: x.interaction_date.dt.to_period('Y')
+        )
 
         logger.info("Save Interaction data in yearly period into CSV files...")
-        self._to_csv(cleaned_dataframe, 'Y', 'yearly')
+        self._to_csv(yearly_dataframe, 'yearly')
 
     def _to_csv(
         self,
         dataframe: dask_dataframe,
-        period_key: str,
-        period_value: str
+        period_type: str
     ) -> None:
         """
-        Slices and saves that `dataframe` into multiple CSV files in a specific `period_type`,
-        starting from that given `start_date` until the given `end_date`.
-
-        Accepted `period_type`:
-        - 'W' : Abbreviation for weekly period
-        - 'M' : Abbreviation for montly period
-        - 'Y' : Abbreviation for yearly period
+        Slices and saves that `dataframe` into multiple CSV files.
         """
-        period_key = period_key.upper()
+        # Check the input directory availability
+        is_exists = os.path.exists(INTERACTION_INPUT_PATH)
 
-        if period_key not in ['W', 'M', 'Y']:
-            raise ValueError("'period_type' is invalid.")
+        if not is_exists:
+            os.makedirs(INTERACTION_INPUT_PATH)
 
-        # Create directory if not exists
-        path = f'{self.INPUT_PATH}/{period_value}'
+        # Check the period directory availability
+        path = f'{INTERACTION_INPUT_PATH}/{period_type}'
 
         is_exists = os.path.exists(path)
 
         if not is_exists:
             os.makedirs(path)
 
-        # Add a new column `period` in that `dataframe`.
-        # The column's value is calculated based on the row's interaction date.
-        dataframe = dataframe.assign(
-            period=lambda x: x.interaction_date.dt.to_period(period_key)
-        )
+        # Slice dataframe into 50 partitions
+        dataframe = dataframe.repartition(npartitions=50)
 
-        # Reset the `dataframe` partition for 100 times.
-        dataframe = dataframe.repartition(npartitions=100)
-
-        # Save to CSV file
-        logger.info(f"Create {period_value} CSV files...")
-        dataframe.to_csv(
-            f'{path}/interactions-*.csv',
-            index=False
+        # Save and simplifies interaction dataframe
+        dataframe[['period', 'organization_id', 'total_thers_in_org', 'therapist_id']].to_csv(
+            f'{path}/{INTERACTION_FILENAME}-part-*.csv',
+            index=False,
+            header=False
         )
 
 
