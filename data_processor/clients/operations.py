c4c6a08a442b9dc5bcf522259666e7aadfe17064
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-29 13:02:40 +0700

    Add all-time num of thers operation

--- data_processor/clients/operations.py
+++ data_processor/clients/operations.py
@@ -3,14 +3,18 @@ import pandas as pd
 import settings
 
 from dask import dataframe as dask_dataframe
-from typing import Dict
+from typing import Dict, List
 
 from clients.api import (
+    AllTimeNumOfTherapistAPI,
     InteractionAPI,
     TherapistAPI,
     NumOfTherapistAPI,
 )
-from clients.mappers import NumOfTherapistMapper
+from clients.mappers import (
+    AllTimeNumOfTherapistMapper,
+    NumOfTherapistMapper,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -18,6 +22,7 @@ logger = logging.getLogger(__name__)
 
 OUTPUT_ACTIVE_INACTIVE_DIRECTORY = 'output/active-inactive'
 
+ALL_TIME_AGGREGATE_FILENAME = 'alltime-aggregate.csv'
 WEEKLY_AGGREGATE_FILENAME = 'weekly-aggregate.csv'
 MONTHLY_AGGREGATE_FILENAME = 'monthly-aggregate.csv'
 YEARLY_AGGREGATE_FILENAME = 'yearly-aggregate.csv'
@@ -115,7 +120,16 @@ class NumOfTherapistBackendOperation:
         self.api = NumOfTherapistAPI()
         self.mapper = NumOfTherapistMapper()
 
-    def sync_back_weekly_data(self) -> None:
+    def sync_back(self) -> None:
+        """
+        Synchronize every active/inactive therapists in the Organization
+        back to the Backend service.
+        """
+        self._sync_back_weekly_data()
+        self._sync_back_monthly_data()
+        self._sync_back_yearly_data()
+
+    def _sync_back_weekly_data(self) -> None:
         """
         Synchronize weekly active/inactive therapists in the Organization
         back to the Backend service.
@@ -126,7 +140,7 @@ class NumOfTherapistBackendOperation:
         for org_id, num_of_thers in num_of_thers_map.items():
             self.api.upsert(org_id, num_of_thers)
 
-    def sync_back_monthly_data(self) -> None:
+    def _sync_back_monthly_data(self) -> None:
         """
         Synchronize monthly active/inactive therapists in the Organization
         back to the Backend service.
@@ -137,7 +151,7 @@ class NumOfTherapistBackendOperation:
         for org_id, num_of_thers in num_of_thers_map.items():
             self.api.upsert(org_id, num_of_thers)
 
-    def sync_back_yearly_data(self) -> None:
+    def _sync_back_yearly_data(self) -> None:
         """
         Synchronize yearly active/inactive therapists in the Organization
         back to the Backend service.
@@ -195,3 +209,37 @@ class NumOfTherapistBackendOperation:
 
         logger.info("Converting number of therapist yearly objects into a dictionary...")
         return self.mapper.to_yearly_therapists_map(dataframe)
+
+
+class AllTimeNumOfTherapistBackendOperation:
+
+    def __init__(self) -> None:
+        self.api = AllTimeNumOfTherapistAPI()
+        self.mapper = AllTimeNumOfTherapistMapper()
+
+    def sync_back(self) -> None:
+        """
+        Synchronize all-time active/inactive therapists in NiceDay
+        back to the Backend service.
+        """
+
+        num_of_thers = self._get_all_time_therapists()
+
+        for num_of_ther in num_of_thers:
+            self.api.upsert(num_of_ther)
+
+    def _get_all_time_therapists(self) -> List[Dict]:
+        """
+        Returns a list of all time active/inactive therapists.
+        """
+
+        logger.info("Importing all-time active/inactive therapists from disk...")
+
+        dataframe = pd.read_csv(
+            f'{OUTPUT_ACTIVE_INACTIVE_DIRECTORY}/{ALL_TIME_AGGREGATE_FILENAME}',
+            sep='\t',
+            header=None
+        )
+
+        logger.info("Converting number of therapist all-time objects into a list...")
+        return self.mapper.to_all_time_therapists(dataframe)
