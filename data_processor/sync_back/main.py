377f8473a4095878982ce2b6121e52fa2c61bf28
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-29 16:02:37 +0700

    Reorganize sync back's codes

---
+++ data_processor/sync_back/main.py
@@ -0,0 +1,35 @@
+import logging
+
+from datetime import datetime
+
+from data_processor.clients.operations import (
+    AllTimeNumOfTherapistBackendOperation,
+    NumOfTherapistBackendOperation,
+)
+from data_processor.helpers import print_time_duration
+from data_processor.settings import configure_logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class SyncBack:
+
+    def __init__(self) -> None:
+        self.all_time_ther_operation = AllTimeNumOfTherapistBackendOperation()
+        self.num_of_ther_operation = NumOfTherapistBackendOperation()
+
+        # Runs data processor
+        process_start_at = datetime.now()
+
+        self.all_time_ther_operation.sync_back()
+        self.num_of_ther_operation.sync_back()
+
+        process_end_at = datetime.now()
+        print_time_duration("Sync back number of therapists", process_start_at, process_end_at)
+
+
+if __name__ == '__main__':
+    configure_logging()
+
+    SyncBack()
