82576b4dc003b30b5942ef4e776dd16957de860a
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-29 15:15:52 +0700

    Add count all therapist reducer

---
+++ data_aggregator/data_aggregator_app/src/main/java/data/aggregator/app/AllTherapistSumReducer.java
@@ -0,0 +1,26 @@
+package data.aggregator.app;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/** A Reducer class that receives inputs from the Mapper class. */
+public class AllTherapistSumReducer extends Reducer<Text, IntWritable, Text, Text> {
+	private Text result = new Text();
+
+	/** Aggregates every value belonging to the key and write it to the HDFS Context. */
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values,  Context context) throws IOException, InterruptedException {
+		int sumThers = 0;
+
+		for (IntWritable val : values) {
+			sumThers += val.get();
+		}
+
+		result.set(String.valueOf(sumThers));
+
+		context.write(key, result);
+	}
+}
