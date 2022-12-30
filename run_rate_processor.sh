11ea65241bd9352b075ec7e906dd15b676a169fe
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-30 17:26:13 +0700

    Add docker-compose to run rates' processor

---
+++ run_rate_processor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TYPE="$1"
+
+if [[ $TYPE =~ ^(locally)$ ]]; then
+    # Load dot env for the sync-back app
+    . .env
+
+    # Overrides BACKEND_URL
+    export BACKEND_URL=http://localhost:8080
+
+    # Registers data processing service module to the python path
+    export PYTHONPATH="${PYTHONPATH}:${PWD}"
+fi
+
+python "$PWD/data_processor/processor_app/rate.py"
