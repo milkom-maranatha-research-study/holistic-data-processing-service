f3c4f408e1b81b7e267793da5da7add2bcbe910a
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-30 13:40:17 +0700

    Modifies `run_aggregator.sh`

--- run_aggregator.sh
+++ run_aggregator.sh
@@ -16,13 +16,16 @@ function prepare_therapist_hadoop_requirements {
     docker cp data_aggregator_app.jar namenode:/tmp
 
     echo "Copy ${PERIOD_TYPE} input files into the 'tmp' dir in the 'namenode' service..."
-    docker cp input/therapist namenode:/tmp/therapist/
+    INPUT_ORG_THERAPIST_PATH="input/org-therapist"
+    INPUT_NAMENODE_ORG_THERAPIST_PATH="tmp/org-therapist"
+
+    docker cp "${INPUT_ORG_THERAPIST_PATH}" namenode:/"${INPUT_NAMENODE_ORG_THERAPIST_PATH}/"
 
     echo "Create HDFS 'input' dir in the 'namenode' service"
     docker exec -it namenode bash hdfs dfs -mkdir /user/root/input
 
-    echo "Copy 'tmp/therapist/' files to the HDFS input dir..."
-    docker exec -it namenode bash hdfs dfs -put tmp/therapist /user/root/input
+    echo "Copy '${INPUT_NAMENODE_ORG_THERAPIST_PATH}' files to the HDFS input dir..."
+    docker exec -it namenode bash hdfs dfs -put "${INPUT_NAMENODE_ORG_THERAPIST_PATH}" /user/root/input
 }
 
 
@@ -30,24 +33,27 @@ function run_therapist_aggregator {
     AGGREGATE_TYPE="$1"
 
     if [[ $AGGREGATE_TYPE =~ ^(all)$ ]]; then
-        echo "Starting 'data_aggregator' service to calculate number of therapists in NiceDay..."
+        echo "Starting 'data_aggregator' service to calculate number of therapists..."
     elif [[ $AGGREGATE_TYPE =~ ^(per-org)$ ]]; then
         echo "Starting 'data_aggregator' service to calculate number of therapists per Organization..."
     fi
 
     echo "Run MR Job..."
-    INPUT_PATH="input/therapist/"
-    OUTPUT_PATH="output/therapist/"
-    docker exec -it namenode hadoop jar tmp/data_aggregator_app.jar data.aggregator.app.TherapistAggregatorDriver "${INPUT_PATH}" "${OUTPUT_PATH}" "${AGGREGATE_TYPE}"
+    INPUT_PATH="input/org-therapist/"
+    OUTPUT_PATH="output/org-therapist/"
+    AGG_JAVA_CLASS="data.aggregator.app.byorg.TherapistAggregatorDriver"
+
+    docker exec -it namenode hadoop jar tmp/data_aggregator_app.jar "${AGG_JAVA_CLASS}" "${INPUT_PATH}" "${OUTPUT_PATH}" "${AGGREGATE_TYPE}"
 
     echo "Export MR Job outputs..."
+    OUTPUT_AGGREGATE_PATH="output/org/num-of-ther"
+    OUTPUT_AGGREGATE_FILENAME="org-${AGGREGATE_TYPE}-aggregate.csv"
 
     # Create 'output' directory if it doesn't exists
-    OUTPUT_AGGREGATE_PATH="output/num_of_ther"
     mkdir -p "${OUTPUT_AGGREGATE_PATH}"
 
-    docker exec -it namenode hadoop fs -getmerge "/user/root/${OUTPUT_PATH}" "${AGGREGATE_TYPE}-aggregate.csv"
-    docker exec -it namenode cat "${AGGREGATE_TYPE}-aggregate.csv" > "${OUTPUT_AGGREGATE_PATH}/${AGGREGATE_TYPE}-aggregate.csv"
+    docker exec -it namenode hadoop fs -getmerge "/user/root/${OUTPUT_PATH}" "${OUTPUT_AGGREGATE_FILENAME}"
+    docker exec -it namenode cat "${OUTPUT_AGGREGATE_FILENAME}" > "${OUTPUT_AGGREGATE_PATH}/${OUTPUT_AGGREGATE_FILENAME}"
 }
 
 
@@ -59,11 +65,12 @@ function cleanup_therapist_aggregator {
     docker exec -it namenode bash hdfs dfs -rm -r /user/root/output
 
     echo "Clean up 'namenode' output file..."
-    docker exec -it namenode rm -f "${AGGREGATE_TYPE}-aggregate.csv"
+    NAMENODE_OUTPUT_FILENAME="org-${AGGREGATE_TYPE}-aggregate.csv"
+    docker exec -it namenode rm -f "${NAMENODE_OUTPUT_FILENAME}"
 
     echo "Clean up 'tmp' files..."
-    TMP_INPUT_PATH="/tmp/therapist"
-    docker exec -it namenode rm -rf "${TMP_INPUT_PATH}"
+    NAMENODE_TMP_INPUT_PATH="/tmp/org-therapist"
+    docker exec -it namenode rm -rf "${NAMENODE_TMP_INPUT_PATH}"
     docker exec -it namenode rm -f /tmp/data_aggregator_app.jar
 }
 
@@ -98,35 +105,15 @@ function prepare_active_therapist_hadoop_requirements {
     docker cp data_aggregator_app.jar namenode:/tmp
 
     echo "Copy ${PERIOD_TYPE} input files into the 'tmp' dir in the 'namenode' service..."
-    if [[ $PERIOD_TYPE =~ ^(alltime)$ ]]; then
-        docker cp input/interaction/alltime namenode:/tmp/alltime-interaction/
-
-    elif [[ $PERIOD_TYPE =~ ^(weekly)$ ]]; then
-        docker cp input/interaction/weekly namenode:/tmp/weekly-interaction/
-
-    elif [[ $PERIOD_TYPE =~ ^(monthly)$ ]]; then
-        docker cp input/interaction/monthly namenode:/tmp/monthly-interaction/
-
-    elif [[ $PERIOD_TYPE =~ ^(yearly)$ ]]; then
-        docker cp input/interaction/yearly namenode:/tmp/yearly-interaction/
-    fi
+    INPUT_ORG_INTERACTION_PATH="input/org-interaction"
+    INPUT_NAMENODE_ORG_INTERACTION_PATH="tmp/${PERIOD_TYPE}-org-interaction"
+    docker cp "${INPUT_ORG_INTERACTION_PATH}/${PERIOD_TYPE}" namenode:/"${INPUT_NAMENODE_ORG_INTERACTION_PATH}/"
 
     echo "Create HDFS 'input' dir in the 'namenode' service"
     docker exec -it namenode bash hdfs dfs -mkdir /user/root/input
 
-    echo "Copy 'tmp/input-${PERIOD_TYPE}-interaction/' files to the HDFS input dir..."
-    if [[ $PERIOD_TYPE =~ ^(alltime)$ ]]; then
-        docker exec -it namenode bash hdfs dfs -put tmp/alltime-interaction /user/root/input
-
-    elif [[ $PERIOD_TYPE =~ ^(weekly)$ ]]; then
-        docker exec -it namenode bash hdfs dfs -put tmp/weekly-interaction /user/root/input
-
-    elif [[ $PERIOD_TYPE =~ ^(monthly)$ ]]; then
-        docker exec -it namenode bash hdfs dfs -put tmp/monthly-interaction /user/root/input
-
-    elif [[ $PERIOD_TYPE =~ ^(yearly)$ ]]; then
-        docker exec -it namenode bash hdfs dfs -put tmp/yearly-interaction /user/root/input
-    fi
+    echo "Copy '${INPUT_NAMENODE_ORG_INTERACTION_PATH}' files to the HDFS input dir..."
+    docker exec -it namenode bash hdfs dfs -put "${INPUT_NAMENODE_ORG_INTERACTION_PATH}" /user/root/input
 }
 
 
@@ -136,18 +123,21 @@ function run_active_therapist_aggregator {
     echo "Starting 'data_aggregator' service to calculate active/inactive therapists..."
 
     echo "Run MR Job on ${PERIOD_TYPE} period..."
-    INPUT_PATH="input/${PERIOD_TYPE}-interaction/"
-    OUTPUT_PATH="output/${PERIOD_TYPE}-interaction/"
-    docker exec -it namenode hadoop jar tmp/data_aggregator_app.jar data.aggregator.app.ActiveTherapistAggregatorDriver "${INPUT_PATH}" "${OUTPUT_PATH}" "${PERIOD_TYPE}"
+    INPUT_PATH="input/${PERIOD_TYPE}-org-interaction/"
+    OUTPUT_PATH="output/${PERIOD_TYPE}-org-interaction/"
+    AGG_JAVA_CLASS="data.aggregator.app.byorg.ActiveTherapistAggregatorDriver"
+
+    docker exec -it namenode hadoop jar tmp/data_aggregator_app.jar "${AGG_JAVA_CLASS}" "${INPUT_PATH}" "${OUTPUT_PATH}" "${PERIOD_TYPE}"
 
     echo "Export MR Job outputs..."
+    OUTPUT_AGGREGATE_PATH="output/org/active-ther-aggregate"
+    OUTPUT_AGGREGATE_FILENAME="org-active-ther-${PERIOD_TYPE}-aggregate.csv"
 
     # Create 'output' directory if it doesn't exists
-    OUTPUT_ACTIVE_INACTIVE_AGGREGATE_PATH="output/active-inactive"
-    mkdir -p "${OUTPUT_ACTIVE_INACTIVE_AGGREGATE_PATH}"
+    mkdir -p "${OUTPUT_AGGREGATE_PATH}"
 
-    docker exec -it namenode hadoop fs -getmerge "/user/root/${OUTPUT_PATH}" "active-inactive-${PERIOD_TYPE}-aggregate.csv"
-    docker exec -it namenode cat "active-inactive-${PERIOD_TYPE}-aggregate.csv" > "${OUTPUT_ACTIVE_INACTIVE_AGGREGATE_PATH}/${PERIOD_TYPE}-aggregate.csv"
+    docker exec -it namenode hadoop fs -getmerge "/user/root/${OUTPUT_PATH}" "${OUTPUT_AGGREGATE_FILENAME}"
+    docker exec -it namenode cat "${OUTPUT_AGGREGATE_FILENAME}" > "${OUTPUT_AGGREGATE_PATH}/${OUTPUT_AGGREGATE_FILENAME}"
 }
 
 
@@ -159,32 +149,33 @@ function cleanup_active_therapist_aggregator {
     docker exec -it namenode bash hdfs dfs -rm -r /user/root/output
 
     echo "Clean up 'namenode' output file..."
-    docker exec -it namenode rm -f "active-inactive-${PERIOD_TYPE}-aggregate.csv"
+    NAMENODE_OUTPUT_FILENAME="org-active-ther-${PERIOD_TYPE}-aggregate.csv"
+    docker exec -it namenode rm -f "${NAMENODE_OUTPUT_FILENAME}"
 
     echo "Clean up 'tmp' files..."
-    TMP_INPUT_PATH="/tmp/${PERIOD_TYPE}-interaction"
-    docker exec -it namenode rm -rf "${TMP_INPUT_PATH}"
+    NAMENODE_INPUT_ORG_INTERACTION_PATH="/tmp/${PERIOD_TYPE}-org-interaction"
+    docker exec -it namenode rm -rf "${NAMENODE_INPUT_ORG_INTERACTION_PATH}"
     docker exec -it namenode rm -f /tmp/data_aggregator_app.jar
 }
 
 
 function active_therapist_aggregator_main {
-    echo "MR Job - All-Time Aggregate..."
+    echo "MR Job - All-time Aggregate..."
     prepare_active_therapist_hadoop_requirements "alltime"
     run_active_therapist_aggregator "alltime"
     cleanup_active_therapist_aggregator "alltime"
 
-    echo "MR Job - Weekly Aggregate..."
+    echo "MR Job - Weekly Aggregate per Organization..."
     prepare_active_therapist_hadoop_requirements "weekly"
     run_active_therapist_aggregator "weekly"
     cleanup_active_therapist_aggregator "weekly"
 
-    echo "MR Job - Monthly Aggregate..."
+    echo "MR Job - Monthly Aggregate per Organization..."
     prepare_active_therapist_hadoop_requirements "monthly"
     run_active_therapist_aggregator "monthly"
     cleanup_active_therapist_aggregator "monthly"
 
-    echo "MR Job - Yearly Aggregate..."
+    echo "MR Job - Yearly Aggregate per Organization..."
     prepare_active_therapist_hadoop_requirements "yearly"
     run_active_therapist_aggregator "yearly"
     cleanup_active_therapist_aggregator "yearly"
