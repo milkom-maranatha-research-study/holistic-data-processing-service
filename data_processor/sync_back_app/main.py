cd554f21638e8e18a7fa3daec1da26003f653f0f
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-30 17:29:51 +0700

    Completing sync back app

--- data_processor/sync_back_app/main.py
+++ data_processor/sync_back_app/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from data_processor.src.clients.operations import (
     AllTimeNumOfTherapistBackendOperation,
     NumOfTherapistBackendOperation,
+    OrganizationRateBackendOperation,
 )
 from data_processor.src.helpers import print_time_duration
 from data_processor.settings import configure_logging
@@ -16,17 +17,20 @@ logger = logging.getLogger(__name__)
 class SyncBack:
 
     def __init__(self) -> None:
+
         self.all_time_ther_operation = AllTimeNumOfTherapistBackendOperation()
         self.num_of_ther_operation = NumOfTherapistBackendOperation()
+        self.organization_rates_operation = OrganizationRateBackendOperation()
 
-        # Runs data processor
+        # Runs sync back operation
         process_start_at = datetime.now()
 
         self.all_time_ther_operation.sync_back()
         self.num_of_ther_operation.sync_back()
+        self.organization_rates_operation.sync_back()
 
         process_end_at = datetime.now()
-        print_time_duration("Sync back number of therapists", process_start_at, process_end_at)
+        print_time_duration("Sync back number of therapists and organizations' rates", process_start_at, process_end_at)
 
 
 if __name__ == '__main__':
