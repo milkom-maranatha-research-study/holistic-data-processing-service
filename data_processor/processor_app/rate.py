40ed7b44e01ad12b6db4e19c4e5e18d62c601501
panjiyudasetya <panjiyudasetya@gmail.com>
2023-01-02 19:16:09 +0700

    Adjusts therapists' rates processor

--- data_processor/processor_app/rate.py
+++ data_processor/processor_app/rate.py
@@ -17,19 +17,19 @@ from data_processor.settings import configure_logging
 logger = logging.getLogger(__name__)
 
 
-ORG_WEEKLY_RATE_INPUT_PATH = 'input/org-rate/weekly'
-ORG_WEEKLY_RATE_INPUT_FILENAME = 'input-rate-weekly'
+ORG_RATE_DIR = 'by-org'
 
-ORG_MONTHLY_RATE_INPUT_PATH = 'input/org-rate/monthly'
-ORG_MONTHLY_RATE_INPUT_FILENAME = 'input-rate-monthly'
+INPUT_RATE_PATH = 'input/rate'
 
-ORG_YEARLY_RATE_INPUT_PATH = 'input/org-rate/yearly'
-ORG_YEARLY_RATE_INPUT_FILENAME = 'input-rate-yearly'
+WEEKLY_RATE_INPUT_FILENAME = 'input-rate-weekly'
+MONTHLY_RATE_INPUT_FILENAME = 'input-rate-monthly'
+YEARLY_RATE_INPUT_FILENAME = 'input-rate-yearly'
 
-ORG_RATE_OUTPUT_PATH = 'output/org/rate'
-ORG_WEEKLY_RATE_OUTPUT_FILENAME = 'org-weekly-rate'
-ORG_MONTHLY_RATE_OUTPUT_FILENAME = 'org-monthly-rate'
-ORG_YEARLY_RATE_OUTPUT_FILENAME = 'org-yearly-rate'
+OUTPUT_RATE_PATH = 'output/rate'
+
+WEEKLY_RATE_OUTPUT_FILENAME = 'output-weekly-rate'
+MONTHLY_RATE_OUTPUT_FILENAME = 'output-monthly-rate'
+YEARLY_RATE_OUTPUT_FILENAME = 'output-yearly-rate'
 
 
 class ActiveTherapistRate:
@@ -118,8 +118,10 @@ class OrgWeeklyActiveTherapistRate(ActiveTherapistRate):
         logger.info("Load Organizations' weekly active therapists data from disk...")
 
         # Step 1 - Load Organizations' weekly active therapists
+        path = f'{INPUT_RATE_PATH}/{ORG_RATE_DIR}/weekly'
+
         dataframe = pd.read_csv(
-            f'{ORG_WEEKLY_RATE_INPUT_PATH}/{ORG_WEEKLY_RATE_INPUT_FILENAME}.csv',
+            f'{path}/{WEEKLY_RATE_INPUT_FILENAME}.csv',
             sep=',',
             names=[
                 'period_start',
@@ -157,7 +159,8 @@ class OrgWeeklyActiveTherapistRate(ActiveTherapistRate):
         )
 
         # Step 3 - Save results into CSV file
-        self._to_csv(dataframe, ORG_RATE_OUTPUT_PATH, ORG_WEEKLY_RATE_OUTPUT_FILENAME)
+        output_path = f'{OUTPUT_RATE_PATH}/{ORG_RATE_DIR}/weekly'
+        self._to_csv(dataframe, output_path, WEEKLY_RATE_OUTPUT_FILENAME)
 
 
 class OrgMonthlyActiveTherapistRate(ActiveTherapistRate):
@@ -182,8 +185,10 @@ class OrgMonthlyActiveTherapistRate(ActiveTherapistRate):
         logger.info("Load Organizations' monthly active therapists data from disk...")
 
         # Step 1 - Load Organizations' monthly active therapists
+        path = f'{INPUT_RATE_PATH}/{ORG_RATE_DIR}/monthly'
+
         dataframe = pd.read_csv(
-            f'{ORG_MONTHLY_RATE_INPUT_PATH}/{ORG_MONTHLY_RATE_INPUT_FILENAME}.csv',
+            f'{path}/{MONTHLY_RATE_INPUT_FILENAME}.csv',
             sep=',',
             names=[
                 'period_start',
@@ -221,7 +226,8 @@ class OrgMonthlyActiveTherapistRate(ActiveTherapistRate):
         )
 
         # Step 3 - Save results into CSV file
-        self._to_csv(dataframe, ORG_RATE_OUTPUT_PATH, ORG_MONTHLY_RATE_OUTPUT_FILENAME)
+        output_path = f'{OUTPUT_RATE_PATH}/{ORG_RATE_DIR}/monthly'
+        self._to_csv(dataframe, output_path, MONTHLY_RATE_OUTPUT_FILENAME)
 
 
 class OrgYearlyActiveTherapistRate(ActiveTherapistRate):
@@ -246,8 +252,10 @@ class OrgYearlyActiveTherapistRate(ActiveTherapistRate):
         logger.info("Load Organizations' yearly active therapists data from disk...")
 
         # Step 1 - Load Organizations' yearly active therapists
+        path = f'{INPUT_RATE_PATH}/{ORG_RATE_DIR}/yearly'
+
         dataframe = pd.read_csv(
-            f'{ORG_YEARLY_RATE_INPUT_PATH}/{ORG_YEARLY_RATE_INPUT_FILENAME}.csv',
+            f'{path}/{YEARLY_RATE_INPUT_FILENAME}.csv',
             sep=',',
             names=[
                 'period_start',
@@ -285,22 +293,35 @@ class OrgYearlyActiveTherapistRate(ActiveTherapistRate):
         )
 
         # Step 3 - Save results into CSV file
-        self._to_csv(dataframe, ORG_RATE_OUTPUT_PATH, ORG_YEARLY_RATE_OUTPUT_FILENAME)
+        output_path = f'{OUTPUT_RATE_PATH}/{ORG_RATE_DIR}/yearly'
+        self._to_csv(dataframe, output_path, YEARLY_RATE_OUTPUT_FILENAME)
 
 
-class TherapistRate:
-
+class TherapistRateProcessor:
     def __init__(self) -> None:
-        OrgWeeklyActiveTherProcessor()
-        OrgMonthlyActiveTherProcessor()
-        OrgYearlyActiveTherProcessor()
+
+        # Runs data processor
+        process_start_at = datetime.now()
+
+        self._run_active_therapist_processor()
 
         OrgWeeklyActiveTherapistRate()
         OrgMonthlyActiveTherapistRate()
         OrgYearlyActiveTherapistRate()
 
+        process_end_at = datetime.now()
+        print_time_duration("Therapists' rates data processing", process_start_at, process_end_at)
+
+    def _run_active_therapist_processor(self):
+        """
+        Run the active therapist data processor.
+        """
+        OrgWeeklyActiveTherProcessor()
+        OrgMonthlyActiveTherProcessor()
+        OrgYearlyActiveTherProcessor()
+
 
 if __name__ == '__main__':
     configure_logging()
 
-    TherapistRate()
+    TherapistRateProcessor()
